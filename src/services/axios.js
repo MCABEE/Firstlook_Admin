efcce39e072494c6e5c992e6522b80757f201826
Mohammed fayis <90344973+mohmd-fayis@users.noreply.github.com>
2023-06-19 07:05:19 +0530

    base url changed to production

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
+const production = "https://firstlook-admin-server.cyclic.app/"
 // eslint-disable-next-line no-unused-vars
-const production = "https://firstlookadminserver.cyclic.app/"
 const development = "http://localhost:8000"
 
 export default axios.create({
-    baseURL: development
+    baseURL: production
 })
