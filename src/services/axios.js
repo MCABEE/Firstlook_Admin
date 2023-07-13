3df687423e0f6b49e243b060e00c5d622009c147
Mohammed fayis <90344973+mohmd-fayis@users.noreply.github.com>
2023-07-13 11:16:15 +0530

    production url updated

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-// eslint-disable-next-line no-unused-vars
 const production = "https://api.controlpanel.firstlook.pro"
+// eslint-disable-next-line no-unused-vars
 const development = "http://localhost:8000"
 
 export default axios.create({
-    baseURL: development
+    baseURL: production
 })
